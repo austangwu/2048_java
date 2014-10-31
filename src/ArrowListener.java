47750604303fa2da3568081b085a1f4ec37efdca
Austin Wu <austin.wu@duke.edu>
2014-10-30 23:25:11 -0400

    First commit

---
+++ src/ArrowListener.java
@@ -0,0 +1,14 @@
+/**
+ * 2048 
+ * Author: Austin Wu 
+ * Completed: June 14th, 2014
+ * 
+ * ArrowListener Interface to listen for key strokes
+ */
+public interface ArrowListener
+{
+	void upPressed();
+	void downPressed();
+	void leftPressed();
+	void rightPressed();
+}
